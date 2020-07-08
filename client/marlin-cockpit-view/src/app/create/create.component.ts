823bc5b0610f385cc13da9a04f28ad1d99693b74
Samrat Saha <aaniruddhaaa@gmail.com>
2020-07-08 16:05:49 +0530

    Converted to Dynamic Steps
    Converted to Dynamic Steps

--- client/marlin-cockpit-view/src/app/create/create.component.ts
+++ client/marlin-cockpit-view/src/app/create/create.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  public stepList = [
+    {
+      stepNo: 1,
+      icon: '',
+      title: 'Base Variant',
+      subTitle: 'Marlin Version Configuration',
+    },
+    {
+      stepNo: 2,
+      icon: '',
+      title: 'Firmware Info',
+      subTitle: 'Boot Screen Configuration',
+    },
+    {
+      stepNo: 3,
+      icon: '',
+      title: 'Hardware Info',
+      subTitle: 'Hardware Details Configuration',
+    },
+    {
+      stepNo: 4,
+      icon: '',
+      title: 'Dimention',
+      subTitle: 'Overall Printer Size',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
